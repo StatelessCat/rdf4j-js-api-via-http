f49e246d2fb5694c50362ef1f26ee89cb6151cd9
Raphaël Cazenave-Lévêque <raphael.cazenave@gmail.com>
2016-09-28 21:28:29 +0200

    Open a Transaction, do a READ query then close the transaction

--- main.js
+++ main.js
@@ -1,24 +1,39 @@
 const http = require('http')
 const querystring = require('querystring')
+const url = require('url')
 
 var constructQ = querystring.stringify({
   'query': 'CONSTRUCT {?t ?s ?r .} WHERE {?t ?s ?r .}'
 })
 
-var doConstructRequest = function (opt) {
+// TODO not application/x-www-form-urlencode ??
+var insertDataQ = querystring.stringify({
+  'update': 'INSERT DATA { <http://exampleSub> <http://examplePred> <http://exampleObj> .}'
+})
+
+var getStatementWithSubject = querystring.stringify({
+  'action': 'GET',
+  'subj': '<http://exampleSub>'
+})
+
+var doRequest = function (opt) {
   return new Promise(function (resolve, reject) {
-    const query = opt.query
+    const query = opt.query || ''
     const verb = opt.verb
+    const path = opt.path
+    const contentType = opt.contentType
     var options = {
       hostname: 'localhost',
       port: 8080,
-      path: '/rdf4j-server/repositories/tsrn',
+      path: path,
       method: verb,
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(query),
-        'Accept': 'application/n-quads'
+        'Accept': 'text/x-nquads',
+        'Content-Length': Buffer.byteLength(query)
       }
+    }
+    if (contentType) {
+      options.headers['Content-Type'] = contentType
     }
     var req = http.request(options, (res) => {
       var responseBody = ''
@@ -35,7 +50,7 @@ var doConstructRequest = function (opt) {
       })
     })
     req.on('error', (e) => {
-      // console.log(`problem with request: ${e.message}`)
+      console.log(`problem with request: ${e.message}`)
       reject(e)
     })
     // write data to request body
@@ -44,7 +59,51 @@ var doConstructRequest = function (opt) {
   })
 }
 
-doConstructRequest({'query': constructQ, verb: 'POST'}).then(function (res) {
-  console.log(res)
+// Read request
+doRequest({
+  path: '/rdf4j-server/repositories/tsrn',
+  'contentType': 'application/x-www-form-urlencoded',
+  'query': constructQ,
+  verb: 'POST'
 })
+  .then(function (res) {
+    console.log(res)
+
+    // open a transaction
+    return doRequest({
+      path: '/rdf4j-server/repositories/tsrn/transactions',
+      'contentType': 'application/x-www-form-urlencoded',
+      verb: 'POST'
+    })
+  })
+  .then(function (res) {
+    // Location response headers contains the URI of the transaction
+    const transactionUri = res.headers.location
+    const transactionUriObject = url.parse(transactionUri)
+    const transactionPathname = transactionUriObject.pathname
+
+    console.log('OPENED transaction: ' + transactionPathname)
+    doRequest({
+      path: transactionPathname + '?' + getStatementWithSubject,
+      verb: 'PUT'
+    })
+      .then(function (res) {
+        console.log('###')
+        console.log(res)
+        console.log('###')
+        return doRequest({path: transactionPathname + '?action=COMMIT', 'query': null, verb: 'PUT'})
+      })
+      .then(function (res) {
+        console.log(res.status === 200)
+        return doRequest({path: transactionPathname + '?action=COMMIT', 'query': null, verb: 'PUT'})
+      })
+      .then(function (res) {
+        // Must be 500 error because transaction already commited
+        console.log(res.status === 500)
+        
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
+  })
 
